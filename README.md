0eda9a33d29d1e8421d06c524a4a4265294ef574
Naudys Reina <nreina@repay.com>
2024-01-12 00:05:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Install webpack as a development dependency
+npm install webpack webpack-cli -D
+# Execute webpack locally in the project (Not being installed globally)
+npx webpack
+# Webpack mode development
+npx webpack --mode development
+# Webpack mode production
+npx webpack --mode production
+# Using a specific config file
+npx webpack --mode production --config webpack.config.js
+# Install babel (Optimize html for all browsers)
+npm install babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime -D
+# Install html webpack (Allows to optimize imports when injecting js, css favicons, etc. With this we don't need to include them on every html)
+npm install html-webpack-plugin -D
+# Install css loader (Allows to optimize imports when injecting css. With this we don't need to include them on every html)
+npm install mini-css-extract-plugin css-loader -D
+# Install stylus loader (Allows to optimize imports when injecting styl. With this we don't need to include them on every html)
+npm install stylus stylus-loader -D
+# Install copy webpack plugin (Copy files when preprocessing)
+npm install copy-webpack-plugin -D
+# Install url and file loader
+npm install url-loader file-loader -D
+npm install css-minimizer-webpack-plugin terser-webpack-plugin -D
+# Install dot env for webpack
+npm install dotenv-webpack -D
+
+# Live server (Plugin for visual code) to create web server for static websites
+
+webpack --mode production
+webpack --mode development
---
+++ webpack.config.dev.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DotEnv = require('dotenv-webpack');
+
+module.exports = {
+    // Specify development mode
+    mode: 'development',
+    // Listen for changes
+    watch: true,
+    // Entrypoint of our app
+    entry: "./src/index.js",
+    output: {
+        clean: true,
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/images/[name].[hash][ext][query]"
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css|\.styl$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            // Optimize files (In this case .png) and generate hashes for every file. This is included in webpack
+            {
+                test: /\.png$/i,
+                type: "asset/resource"
+            },
+            // Optimize files (In this case .woff and .woff2) and generate hashes for every file. This is included in webpack
+            {
+                test: /\.(woff|woff2)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name].[hash][ext][query]"
+                }
+                /*use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    }
+                }*/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        /*new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })*/
+        new DotEnv()
+    ]
+}
